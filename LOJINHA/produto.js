a335d61dde4d202d06d99a5137bbf6d00e087f00
David Gabriel <95094763+DavidznX@users.noreply.github.com>
2025-04-19 23:19:33 -0300

    Add files via upload

--- LOJINHA/produto.js
+++ LOJINHA/produto.js
@@ -3,15 +3,17 @@ fetch('produtos.json')
   .then(produtos => {
     const container = document.getElementById('lista-produtos');
 
-    produtos.forEach(produto => {
+    produtos.forEach((produto, index) => {
       const div = document.createElement('div');
       div.className = 'produto';
 
       div.innerHTML = `
+        <div class='produtoo'>
         <h2>${produto.nome}</h2>
         <img src="${produto.imagem}" alt="${produto.nome}">
         <p><strong>Preço:</strong> ${produto.preco}</p>
-        <a href="${produto.linkAfiliado}" target="_blank">Ver na Amazon</a>
+        <a href="produto_detalhes.html?id=${index}">Ver</a>
+        </div>
       `;
 
       container.appendChild(div);
